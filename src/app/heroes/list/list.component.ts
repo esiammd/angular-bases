20530f98a34230e0574b7cb2e0dac985543753b1
esiammd <esiammd@hotmail.com>
2024-12-11 18:02:04 +0100

    Fin de sección 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  standalone: false,
+
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor'
+  ];
+  public deleteHero?: string;
+
+  removeLastHeroe(): void {
+    this.deleteHero = this.heroNames.pop();
+  }
+}
